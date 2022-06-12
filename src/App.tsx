ea55d5d11d60320d1cc7e99ec85c40c5e670ec7b
박지윤 <jyun1937@naver.com>
2022-06-12 22:47:51 +0900

    make basic template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import { Helmet } from "react-helmet";
+
+import "./App.css";
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+	*{
+		box-sizing:border-box;
+	}
+	body,html{
+		width:100vw;
+		overflow-x:hidden;
+	}
+	body::-webkit-scrollbar{
+		display:none;
+	}
+	body {
+		font-family:'Source Sans Pro', sans-serif;
+	}
+	a{
+		color:inherit;
+		text-decoration:none;
+	}
+	li{
+		list-style:none;
+	}
+	button{
+		margin:0;
+		padding:0;
+		background: transparent;
+		border:none;
+	}
+
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Helmet title="Trello" />
+			<GlobalStyle />
+		</>
+	);
 }
 
 export default App;
